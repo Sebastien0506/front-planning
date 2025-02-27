3bdb4eaec00ba985bd2c7973c5a2d37e34c92489
Sebastien0506 <124361814+Sebastien0506@users.noreply.github.com>
2025-02-27 17:15:32 +0100

    Modification des couleur du front

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,9 +1,14 @@
 import Link from "next/link"
+import { Poppins, Ranga } from "next/font/google";
+
+const poppins = Poppins({subsets: ['latin'], weight: ["600"]});
+const ranga = Ranga({subsets: ["latin"], weight: ["700"]})
 
 function NavBar(){
     return(
-        <nav>
-            <ul>
+        <nav style={{display: "flex", flexDirection:"row", justifyContent: "space-around", backgroundColor:"#171717"}}>
+            <span className={ranga.className} style={{color: "white", fontSize: "2.5rem", marginLeft: "5%"}}>Planeasy</span>
+            <ul className={poppins.className} style={{color: "white", fontSize: "1.5rem", width: "50%" ,display: "flex", justifyContent: "space-around", margin: "auto"}}>
                 <li> <Link href="/">Accueil</Link></li>
                 <li> <Link href="/connexion">Connexion</Link></li>
                 <li> <Link href="/inscription">Inscription</Link></li>
